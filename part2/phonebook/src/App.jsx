aef5f63b846668d77c08e8615b1ded97422e1508
totoargumedo <totoargumedo@gmail.com>
2024-12-04 11:34:51 -0300

    exercise part2 2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -7,6 +7,13 @@ const App = () => {
   const addPerson = (event) => {
     event.preventDefault();
 
+    const nameExists = persons.some((person) => person.name === newName);
+
+    if (nameExists) {
+      alert(`${newName} is already added to the phonebook`);
+      return;
+    }
+
     const personObject = {
       name: newName,
     };
