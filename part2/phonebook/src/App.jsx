ce4d16148801164a33174cd8b543370ed5e1a612
totoargumedo <totoargumedo@gmail.com>
2024-12-06 10:55:23 -0300

    exercise part2 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,17 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import PhoneNumberList from "./components/PhonenumberList";
 import SearchPerson from "./components/SearchPersonForm";
 import AddPersonForm from "./components/AddPersonForm";
 import Subtitle from "./components/Subtitle";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      const personsGet = response.data;
+      setPersons(personsGet);
+    });
+  }, []);
 
   const filteredPersons = persons.filter((person) =>
     person.name.toLowerCase().includes(filter.toLowerCase())
